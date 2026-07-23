b63ffd47c5b3177158aa80da2f68bbd62caa28f5
omar <omar.i.s.diab@gmail.com>
2026-07-23 04:51:57 +0300

    Finish from Home Design

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withViewTransitions()),
     provideClientHydration(),
     provideHttpClient(withFetch()),
+    // importProvidersFrom(BrowserAnimationsModule),
+    provideAnimations(),
   ],
 };
--- src/app/components/pages/blanks/home/home.component.ts
+++ src/app/components/pages/blanks/home/home.component.ts
@@ -1,31 +1,92 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ProductsService } from '../../../../core/services/products/products.service';
 import { IProducts } from '../../../../core/interfaces/iproducts';
+import { Subscription } from 'rxjs';
+import { CategoriesService } from '../../../../core/services/categories/categories.service';
+import { ICategories } from '../../../../core/interface/icategories';
+
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './home.component.html',
-  styles: ``
+  styles: ``,
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   private readonly _ProductsService = inject(ProductsService);
+  private readonly _CategoriesService = inject(CategoriesService);
 
-  productsList:IProducts[] = [];
+  productsList: IProducts[] = [];
+  categoriesList: ICategories[] = [];
+
+  getAllProductsSub!: Subscription;
+
+  customOptionsCategories: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    dots: true,
+    navSpeed: 700,
+    navText: ['<', '>'],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 6,
+      },
+    },
+    nav: false,
+};
+
+  customOptionsSlider: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    items: 1,
+    nav: true,
+};
 
   ngOnInit(): void {
-    this._ProductsService.getAllProducts().subscribe({
-      next:(res) => {
-        this.productsList = res.data
-        console.log(res.data)
+    this.getAllProductsSub = this._ProductsService.getAllProducts().subscribe({
+      next: (res) => {
+        this.productsList = res.data;
       },
-      error:(err) => {
-        console.log(err)
-      }
-    })
+      error: (err) => {
+        console.log(err);
+      },
+    });
+
+    this._CategoriesService.getAllCategories().subscribe({
+      next: (res) => {
+        this.categoriesList = res.data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
-
-
+  ngOnDestroy(): void {
+    this.getAllProductsSub?.unsubscribe();
+  }
 }
